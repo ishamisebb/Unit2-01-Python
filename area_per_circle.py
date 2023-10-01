80ac3b91540d774cac7f65b6ac3c3410abafc9c0
Ishami Sebgoya <ishami.sebgoya@stu.ocsb.ca>
2023-10-01 19:46:01 +0000

    Added area_per_circle.py

---
+++ area_per_circle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Ishami Sebgoy
+# Created on: September. 30, 2023
+# This program calculates and displays the area and perimeter of a 
+# circle with radius 12 mm.
+import math
+
+def main():
+    print ("For a circle that has a radius")
+    print ("of 12 mm:")
+    print ("")
+    print ("Area = {} mm^2". format (math.pi*12**2))
+    print ("Perimeter = {} mm". format (2*math.pi*12))
+
+
+if __name__ == "__main__":
+  main()
